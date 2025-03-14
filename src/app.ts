4f2b8fdef8aaf8f2612714c96a2981755ec9d100
Khun <lykhun@gmail.com>
2025-03-14 14:58:28 +0100

    add swagger

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import express from "express";
 import { routes } from "./routes";
 import db from "./models";
 import { JwtMiddleware } from "./middlewares/jwt.middleware";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDoc from '../swagger.json'; 
 
 db.sequelize.sync();
 
 const app = express();
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use(express.json());
 app.use(JwtMiddleware);
